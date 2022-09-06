ca51b98093af08517ae54906323a60c3c03af13c
19039 <1903994095@qq.com>
2022-09-06 13:50:01 +0800

    feat: 平台不同，获取IP的方法不同。发布0.0.2版本。

--- lib/src/util/net_utils.dart
+++ lib/src/util/net_utils.dart
@@ -14,11 +14,38 @@ class NetUtils {
 
   /// 获取无线网的局域网IP（后续需要完善下网线连接的情况，以太网，Ethernet）
   static Future<String> getIpOfWLANOrEthernet() async {
+    if (Platform.isWindows) {
+      return await _getIpOnWindows();
+    } else if (Platform.isAndroid) {
+      return await _getIpOnAndroid();
+    } else {
+      return '';
+    }
+  }
+
+  /// 获取windows的IP
+  static Future<String> _getIpOnWindows() async {
     late String ip;
     for (var interface in await NetworkInterface.list()) {
       final name = interface.name;
       if (name.contains('WLAN') ||
           name.contains('Ethernet') && !name.contains('vEthernet')) {
+        for (var address in interface.addresses) {
+          if (address.type.name == 'IPv4') {
+            ip = address.address;
+          }
+        }
+      }
+    }
+    return ip;
+  }
+
+  /// 获取Android的IP
+  static Future<String> _getIpOnAndroid() async {
+    late String ip;
+    for (var interface in await NetworkInterface.list()) {
+      final name = interface.name;
+      if (name.contains('wlan0')) {
         for (var address in interface.addresses) {
           if (address.type.name == 'IPv4') {
             ip = address.address;
