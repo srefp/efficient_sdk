740622f98e189e69afe07748e69020ea04281167
19039 <1903994095@qq.com>
2022-09-20 15:19:34 +0800

    feat: 添加拼音测试

---
+++ example/test/pinyin_test.dart
@@ -0,0 +1,10 @@
+import 'package:efficient_sdk/efficient_sdk.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('测试拼音', () {
+    print('奇怪'.pinyinFirstLetter);
+    print('奇数'.pinyinFirstLetter);
+    print('调度室'.pinyinFirstLetter);
+  });
+}
